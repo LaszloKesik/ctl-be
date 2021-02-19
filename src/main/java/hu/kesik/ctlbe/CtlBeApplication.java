e1d8fac175603f2af063f30f2d03c4ee7982cda6
lkesik <laszlo@kesik.hu>
2021-02-19 21:54:31 +0100

    Initial commit

---
+++ src/main/java/hu/kesik/ctlbe/CtlBeApplication.java
@@ -0,0 +1,13 @@
+package hu.kesik.ctlbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtlBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtlBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/hu/kesik/ctlbe/ServletInitializer.java
@@ -0,0 +1,13 @@
+package hu.kesik.ctlbe;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CtlBeApplication.class);
+	}
+
+}
